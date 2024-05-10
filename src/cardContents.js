09a3721ab08d1cacb625b8d07cb9d21e589ffb34
meet patel <meetkpatel2210@gmail.com>
2024-05-10 19:10:51 -0400

    Just changed the color and watching the deployment

--- src/cardContents.js
+++ src/cardContents.js
@@ -4,21 +4,21 @@ export const projects = [
       description: "In the realm of fitness, Gym Blog Website offers a wealth of knowledge on workouts, nutrition, and more. Explore our content to enhance your fitness journey.",
       src: require("./static/Project/Jootabajar.png"),
       link: "https://www.ignant.com/2023/03/25/ad2186-matthias-leidingers-photographic-exploration-of-awe-and-wonder/",
-      color: "#d4aca7"
+      color: "#304D30"
     },
     {
       title: "Jootabajar - Your One-Stop Shoe Store",
       description: "In the realm of footwear, Jootabajar offers a plethora of options to suit your style and needs. Explore our extensive collection to find the perfect pair for every occasion.",
       src: require("./static/Project/Gym-Blog.png"),
       link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
-      color: "#977F6D"
+      color: "#88AB8E"
     },
     {
       title: "Zissou",
       description: "Though he views photography as a medium for storytelling, Zissou’s images don’t insist on a narrative. Both crisp and ethereal, they’re encoded with an ambiguity—a certain tension—that lets the viewer find their own story within them.",
       src: require("./static/Images/Parralax/Trees.jpg"),
       link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
-      color: "#C2491D"
+      color: "#AFC8AD"
     },
   ];
   
